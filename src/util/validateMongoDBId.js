ee40780596b684b1137aeeebba20e59c9aeff014
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-02-22 01:00:23 +0600

    refreshed

--- src/util/validateMongoDBId.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const validateMongoDBId = (id) =>{
-    const isValid = mongoose.Schema.Types.ObjectId(id);
-    if(!isValid) throw new Error("This id is not valid or not Found");
-}
-
-export {validateMongoDBId};
