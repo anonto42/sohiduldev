1fcfb99be44f0c3b362f0a965cd929a5a3f45ef3
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-02-22 18:32:38 +0600

    created the naveber

--- src/components/Naveber/Naveber.tsx
+++ src/components/Naveber/Naveber.tsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import React from 'react';
+import Sidebar from '../SideBar/Sidebar';
+import Link from 'next/link';
 
 const Naveber = () => {
   return (
-    <div>Naveber</div>
+    <div className='relative w-full bg-[#ffff0000]'>
+        <div className='h-[80px] w-full relative flex text-textColor justify-center items-center md:hidden px-5'> 
+            <Link href="/" className=''>
+                <h1 className='flex relative uppercase font-bold italic text-[18px]'>sohidul Islam Ananto
+                  <div className='w-[3px] absolute bottom-1 -right-2 rounded-full h-[3px] bg-highlighte'/>
+                </h1>
+            </Link>
+        </div>
+        <Sidebar />
+        <div className='hidden md:block w-full mx-auto'>
+            <div className='w-full h-[80px] flex text-textColor justify-between items-center'>
+                <div id='logo' className=''>
+                    <Link href="/">
+                        <h1 className='flex relative uppercase font-bold italic text-[22px]'>sohidul Islam Ananto
+                          <div className='w-[3px] absolute bottom-1 -right-2 rounded-full h-[3px] bg-highlighte'/>
+                        </h1>
+                    </Link>
+                </div>
+                <div className='flex'>
+                    <Link href="/">
+                        <div className='mr-4 w-[105px] h-[40px] flex justify-center items-center rounded-full hover:text-[#2e2e2e] text-[17px] font-semibold duration-150 ease-linear cursor-pointer hover:bg-highlighte hover:shadow-md hover:shadow-[#ffffff7b]'>Home</div>
+                    </Link>
+                    <Link href="/about">
+                        <div className='mx-4 w-[105px] h-[40px] flex justify-center items-center rounded-full hover:text-[#2e2e2e] text-[17px] font-semibold duration-150 ease-linear cursor-pointer hover:bg-highlighte hover:shadow-md hover:shadow-[#ffffff7b]'>About Me</div>
+                    </Link>
+                    <Link href="/project">
+                        <div className='mx-4 w-[105px] h-[40px] flex justify-center items-center rounded-full hover:text-[#2e2e2e] text-[17px] font-semibold duration-150 ease-linear cursor-pointer hover:bg-highlighte hover:shadow-md hover:shadow-[#ffffff7b]'>Projects</div>
+                    </Link>
+                    <Link href="/contact">
+                        <div className='ml-4 w-[125px] h-[40px] flex justify-center items-center rounded-full text-[#2e2e2e] text-[17px] font-semibold duration-150 ease-linear cursor-pointer bg-highlighte shadow-md shadow-[#ffffff7b]'>Contact me</div>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
---
+++ src/components/SideBar/Sidebar.tsx
@@ -0,0 +1,34 @@
+"use client"
+import Link from 'next/link';
+import React, { useState } from 'react';
+import { FaBars} from 'react-icons/fa6'
+
+const Sidebar = () => {
+    const [nav,setNav]= useState<boolean>(false);
+    const navFunction=()=>{
+        nav === false ? setNav(true) : setNav(false); 
+    }
+  return (
+    <div className=''>
+        <div className={nav === true ? 'bg-highlighte nav-container shadow-md shadow-[#ffffff7b] w-[250px] sm:w-[330px] md:hidden h-[200px] rounded-md fixed z-50 duration-200 ease-linear top-2' : 'bg-highlighte nav-container shadow-md shadow-[#ffffff7b] w-[250px] sm:w-[330px] md:hidden h-[200px] rounded-md absolute z-50 duration-200 ease-linear -top-[220px]'}>
+            <Link href="/">
+                    <h3 className='hover:bg-[#232323] duration-300 ease-out hover:px-3 hover:text-textColot font-semibold cursor-pointer px-1 my-2 py-2 w-[95%] mx-auto rounded-xl text-[16px]'>Home</h3>
+            </Link>
+            <Link href="/about">
+                    <h3 className='hover:bg-[#232323] duration-300 ease-out hover:px-3 hover:text-white font-semibold cursor-pointer px-1 my-2 py-2 w-[95%] mx-auto rounded-xl text-[16px]'>About Me</h3>
+            </Link>
+            <Link href="/project">
+                    <h3 className='hover:bg-[#232323] duration-300 ease-out hover:px-3 hover:text-textColor font-semibold cursor-pointer px-1 my-2 py-2 w-[95%] mx-auto rounded-xl text-[16px]'>Project</h3>
+            </Link>
+            <Link href="/contact">
+                    <h3 className='hover:bg-[#232323] duration-300 ease-out hover:px-3 hover:text-textColor font-semibold cursor-pointer px-1 my-2 py-2 w-[95%] mx-auto rounded-xl text-[16px]'>Contact me</h3>
+            </Link>
+        </div>
+        <div onClick={()=>navFunction()} className='md:hidden right-3 top-6 fixed w-[35px] h-[30px] cursor-pointer bg-highlighte shadow-md shadow-[#ffffff7b] z-50 rounded-xl flex justify-center items-center text-textColot text-[22px] font-thin active:scale-[1.35] duration-[1s]'>
+                <FaBars/>
+            </div>
+    </div>
+  )
+}
+
+export default Sidebar
