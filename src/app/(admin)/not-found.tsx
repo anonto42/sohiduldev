8d7081e07b60215f7563f72351811d0a7992c6a9
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-02-22 16:24:34 +0600

    not found page

---
+++ src/app/(admin)/not-found.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NotFoundPage = () => {
+  return (
+    <div>This is the not found page... (from admin)</div>
+  )
+}
+
+export default NotFoundPage
---
+++ src/app/(general)/not-found.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NotFoundPage = () => {
+  return (
+    <div>This is the not found page... (from general)</div>
+  )
+}
+
+export default NotFoundPage
