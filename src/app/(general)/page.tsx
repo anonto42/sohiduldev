7047dfc9e5d525b8e412f804c24ddc86b7c54935
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-02-23 00:20:10 +0600

    added all components of the home page

--- src/app/(general)/page.tsx
+++ src/app/(general)/page.tsx
@@ -1,8 +1,21 @@
+import Contact from '@/components/Contact/Contact'
+import Introduction from '@/components/Introduction/Introduction'
+import Section from '@/components/ProjectSection/Section'
+import Services from '@/components/ServicesInformation/Services'
 import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>Home page</div>
+    <div>
+      {/* Introduction */}
+      <Introduction />
+      {/* Services Information */}
+      <Services />
+      {/* Projects show case */}
+      <Section />
+      {/* Contact From */}
+      <Contact />
+    </div>
   )
 }
 
