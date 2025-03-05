9a1e994185fa410039786f0f9aecb2900994f9ec
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-03-05 13:07:53 +0600

    added new acces poient

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images:{
-    remotePatterns:[{
-      protocol:"https",
-      hostname:"img.freepik.com"
-    }]
+    remotePatterns:[
+      {
+        protocol:"https",
+        hostname:"img.freepik.com"
+      },
+      {
+        protocol:"http",
+        hostname:"res.cloudinary.com"
+      }
+    ]
   }
   /* config options here */
 };
