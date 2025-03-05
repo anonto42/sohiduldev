26d738afa0e8c229f06d298f23be06031660d427
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-03-05 13:48:12 +0600

    hight updated

--- src/components/ServicesInformation/Services.tsx
+++ src/components/ServicesInformation/Services.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 const Services = () => {
   return (
-    <div className='min-h-[20vh] w-full text-white'>
+    <section className='h-[100px] w-full text-white'>
         <div className='w-full flex justify-center mt-20 mb-[50px] text-center text-topr'>
             <div>
                 <h1 className='text-[30px] md:text-[40px] font-bold'>I will develop</h1>
                 <h4 className='text-[25px] md:text-[30px] font-semibold italic text-highlighte -mt-3'>a website tailored to your specific needs.</h4>
             </div>
         </div>
-    </div>
+    </section>
   )
 }
 
