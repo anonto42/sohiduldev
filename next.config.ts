fabf7369ade7afe561fbc12f3cae2c7d84a65891
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-03-05 12:57:48 +0600

    adding extra image od data resorcess

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images:{
+    remotePatterns:[{
+      protocol:"https",
+      hostname:"img.freepik.com"
+    }]
+  }
   /* config options here */
 };
 
