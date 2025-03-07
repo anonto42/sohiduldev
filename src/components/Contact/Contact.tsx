3c704a51ff6549029e2b8224318101f6db098f3a
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-03-07 15:36:53 +0600

    updated the contct and the form

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { FaLocationDot } from 'react-icons/fa6';
 import { MdEmail } from 'react-icons/md';
+import Form from './Form';
 
 const Contact = () => {
   return (
-    <div className='w-full h-auto mb-9 text-textColor md:text-center'>
+    <section className='w-full h-auto mb-9 text-textColor md:text-center'>
         {/* contact */}
        <div className='mb-6'>
         <div className='text-center'>
@@ -30,22 +31,8 @@ const Contact = () => {
         </div>
        </div>
        {/* from */}
-       <div >
-        <div className='w-full mb-4'>
-          <h1 className='font-thin'>Your name</h1>
-          <input placeholder='Your Name' type="text" className='w-full md:w-[700px] outline-none h-[60px] border bg-[#141427] rounded-full px-4 mt-3 placeholder:font-serif'/>
-        </div>
-        <div className='w-full mb-4'>
-          <h1 className='font-thin'>Your email</h1>
-          <input placeholder='Your Email' type="email" className='w-full md:w-[700px] outline-none h-[60px] border bg-[#141427] rounded-full px-4 mt-3 placeholder:font-serif'/>
-        </div>
-        <div className='w-full mb-4'>
-          <h1 className='font-thin'>What do you want to do</h1>
-          <textarea rows={7} cols={40} placeholder='Write here...' className='w-full outline-none md:w-[700px] py-2 border bg-[#141427] rounded-xl px-4 mt-3 placeholder:font-serif'/>
-        </div>
-        <button className='bg-highlighte text-[#141427] font-bold px-5 py-4 rounded-xl text-md mb-4 active:scale-105 duration-150 ease-in-out'>Send Message</button>
-       </div>
-      </div>
+       <Form />
+      </section>
   )
 }
 
