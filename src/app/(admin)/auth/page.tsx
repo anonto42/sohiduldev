aaaf6d7d8c5c4f1bb1e7f2d19fe9c461b5b3ebe2
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-03-07 13:20:42 +0600

    updated the design

--- src/app/(admin)/auth/page.tsx
+++ src/app/(admin)/auth/page.tsx
@@ -7,7 +7,7 @@ const page = () => {
         className='w-full min-h-svh flex justify-center items-center'
     >
         <div 
-            className='w-full h-svh md:w-[400px] md:h-[550px] bg-[#ffffff13] md:rounded-lg shadow-lg shadow-black flex justify-center items-center'
+            className='w-full h-full md:h-0 py-20 md:w-[400px] bg-[#ffffff13] md:rounded-lg shadow-lg shadow-black flex justify-center items-center'
         >
             <div
                 className=''
@@ -15,12 +15,15 @@ const page = () => {
                 <h2
                     className='text-textColor text-3xl pb-8'
                 >
-                    Connect with github
+                    Login with GitHub
                 </h2>
 
                     <button
-                        className='bg-[#010409] flex items-center justify-between gap-2 py-2 px-4 rounded-lg mx-auto active:bg-[#00000096] active:scale-110 duration-100 ease-in-out shadow-md shadow-[black]'
-                    >GitHub <FaGithub /></button>
+                        className='bg-[#010409] flex items-center justify-between gap-2 py-2 px-4 rounded-lg mx-auto active:bg-[#00000096] active:scale-[1.03] duration-100 ease-in-out shadow-md hover:shadow-[#0000005b]'
+                    >
+                        GitHub 
+                        <FaGithub />
+                    </button>
             
             </div>
         </div>
