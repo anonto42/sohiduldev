fa4fc5b15d0a46e8d0431dc28614b976e5237864
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-03-07 14:45:06 +0600

    updated the context

--- src/components/ServicesInformation/Services.tsx
+++ src/components/ServicesInformation/Services.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Services = () => {
   return (
-    <section className='h-[100px] w-full text-white'>
-        <div className='w-full flex justify-center mt-20 mb-[50px] text-center text-topr'>
-            <div>
-                <h1 className='text-[30px] md:text-[40px] font-bold'>I will develop</h1>
-                <h4 className='text-[25px] md:text-[30px] font-semibold italic text-highlighte -mt-3'>a website tailored to your specific needs.</h4>
+    <section className='h-[100px] w-full'>
+        <div className='w-full flex justify-center my-20 text-center text-topr'>
+            <div className=' bg-gradient-to-br from-white to-highlighte bg-clip-text text-transparent'>
+                <h1 className='text-2xl md:text-4xl lg:text-5xl font-bold'>I will create bespoke software solutions</h1>
+                <h4 className='text-2xl md:text-3xl lg:text-4xl font-semibold'>precisely tailored to meet your specific needs.</h4>
             </div>
         </div>
     </section>
