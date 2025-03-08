73ee66d60b7841858a0169b70427506412312071
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-03-08 13:08:55 +0600

    added the project show section

--- src/components/Deshbord_Items/ProjectOpt.tsx
+++ src/components/Deshbord_Items/ProjectOpt.tsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import SingleProject from './SingleProject'
+
+export interface ProjecInterface{
+  title: string;
+  liveLink: string;
+  frontImage: string;
+}
 
 const ProjectOpt = () => {
+
+  const projects: ProjecInterface[] = [
+    {title:"Project",liveLink:"...",frontImage:"lll"},
+    {title:"Project",liveLink:"...",frontImage:"lll"},
+    {title:"Project",liveLink:"...",frontImage:"lll"},
+    {title:"Project",liveLink:"...",frontImage:"lll"},
+    {title:"Project",liveLink:"...",frontImage:"lll"},
+    {title:"Project",liveLink:"...",frontImage:"lll"},
+    {title:"Project",liveLink:"...",frontImage:"lll"},
+    {title:"Project",liveLink:"...",frontImage:"lll"},
+    {title:"Project",liveLink:"...",frontImage:"lll"},
+    {title:"Project",liveLink:"...",frontImage:"lll"},
+    {title:"Project",liveLink:"...",frontImage:"lll"},
+  ]
+
   return (
-    <section className='w-full h-svh p-6'>ProjectOpt</section>
+    <section className='w-full h-svh'>
+      <div className='p-6 w-full h-full overflow-y-auto grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-6'>
+        {
+          projects.map((item,index)=><SingleProject project={item} key={index} />)
+        }
+      </div>
+    </section>
   )
 }
 
