8e18ded497c95fe31bd1966a17d33ea02617d8c1
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-03-08 11:18:10 +0600

    created the options for controle

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,8 +1,18 @@
-import React from 'react'
+"use client"
+import Bar from '@/components/Deshbord_Items/Bar';
+import Messages from '@/components/Deshbord_Items/Messages';
+import ProjectOpt from '@/components/Deshbord_Items/ProjectOpt';
+import React, { useState } from 'react';
 
 const page = () => {
+  const [opt,setOpt] = useState(true);
   return (
-    <div>admin deshboard page</div>
+    <div className='flex'>
+      {/* side bar for the univarsel think */}
+      <Bar setWork={setOpt} />
+      {/* messages options */}
+      { opt? <Messages /> : <ProjectOpt /> }
+    </div>
   )
 }
 
