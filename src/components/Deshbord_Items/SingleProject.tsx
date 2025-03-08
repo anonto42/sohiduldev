cd7c95099998feabb0bd32bbf8283818741e79a3
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-03-08 13:10:30 +0600

    created the singel project component

---
+++ src/components/Deshbord_Items/SingleProject.tsx
@@ -0,0 +1,40 @@
+"use client"
+import Image from 'next/image'
+import React from 'react'
+import { ProjecInterface } from './ProjectOpt'
+
+const SingleProject = ({project}:{project:ProjecInterface}) => {
+
+    const deleteProject = async () => {}
+    const editeProject = async () => {}
+
+  return (
+    <div className='bg-[#31313171] rounded-lg shadow-[0px_0px_5px] p-3'>
+        <div className='w-full h-[120px] relative rounded-md bg-gray-500/60'>
+            <Image 
+                src={"/images/defualt-blur-image.webp"} 
+                alt={project.title}
+                blurDataURL="/images/defualt-blur-image.webp"
+                placeholder='blur'
+                fill
+                objectFit='contain'
+                loading='lazy'                
+                quality={80}
+                className='object-cover'
+            />
+        </div>
+        <h1 className='mt-2 text-lg font-light'>{project.title}</h1>
+        <h3 className='mb-2 mt-1 text-sm'>{project.liveLink}</h3>
+        <div className='w-full flex gap-4 justify-between'>
+            <button 
+                onClick={()=>deleteProject()}
+                className='py-2 px-5 rounded-md bg-red-700 active:scale-[1.02] ease-in-out duration-100'>Delete</button>
+            <button 
+                onClick={()=>editeProject()}
+                className='py-2 px-5 rounded-md bg-blue-700 active:scale-[1.02] ease-in-out duration-100'>Edit</button>
+        </div>
+    </div>
+  )
+}
+
+export default SingleProject
