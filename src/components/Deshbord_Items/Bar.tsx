f04cdd65d2ebfd4a624cffc36099f39b71fa33c2
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-03-08 11:16:53 +0600

    creating the navigation

--- src/components/Deshbord_Items/Bar.tsx
+++ src/components/Deshbord_Items/Bar.tsx
@@ -7,13 +7,13 @@ const Bar = ({setWork}:{setWork:any}) => {
   return (
     <nav className='w-[100px] p-6 h-svh bg-[#0507257e] shadow-[0px_0px_10px_rgb(2,3,4,0.4)] text-3xl'>
         <div 
-            onClick={()=>setWork("message")}
+            onClick={()=>setWork(true)}
             className='rounded-full py-2 mb-6 cursor-pointer flex justify-center items-center hover:bg-[#8686866b] hover:shadow-[0px_0px_5px_rgb(255,255,255,0.5)] duration-100 ease-in-out active:scale-90'
         >
           <AiFillMessage />
         </div>
         <div 
-            onClick={()=>setWork("projects")}
+            onClick={()=>setWork(false)}
             className='rounded-full py-2 mb-6 cursor-pointer flex justify-center items-center hover:bg-[#8686866b] hover:shadow-[0px_0px_5px_rgb(255,255,255,0.5)] duration-100 ease-in-out active:scale-90'
         >
           <RiProjectorFill />
