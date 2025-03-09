719d3b800e07f2d886da40b8ff9bde589deaca15
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-03-09 18:59:55 +0600

    added the logout

--- src/app/actions/index.ts
+++ src/app/actions/index.ts
@@ -1,9 +1,11 @@
 "use server"
-import { signIn } from "@/auth"
+import { signIn, signOut } from "@/auth"
 
 export async function gitHubLogin(formData:any){
     const action = formData.get("action")
     await signIn(action,{ redirectTo:"/dashboard" })
 }
 
-export async function logOut(formData:any){}
+export async function logOut(formData:any){
+    await signOut({redirectTo:"/"})
+}
