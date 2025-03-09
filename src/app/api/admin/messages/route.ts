ade1114267fae3517989ca625e1d79e015354e4e
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-03-09 14:33:14 +0600

    created the delete message route

--- src/app/api/admin/messages/route.ts
+++ src/app/api/admin/messages/route.ts
@@ -15,3 +15,19 @@ export async function GET(request: Request) {
         return ApiResponse.error("Something went wrong!", 500);
     }
 }
+
+export async function DELETE(request: Request) {
+    await MongoConnect();
+    try {
+        
+        const { id } = await request.json();
+        if (!id || id.trim() === "") return ApiResponse.error("Select an id for delete a message.")
+
+        await MessageModel.deleteOne({_id:id});
+        return ApiResponse.success("Successfully delete the message.",200);
+
+    } catch (error) {
+        console.error("Error:", error);
+        return ApiResponse.error("Something was wrong! on deleting the message", 500);
+    }
+}
