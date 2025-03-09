b333972aa9f36c4053f2cecbb33811cfa4033405
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-03-09 12:44:31 +0600

    classname is not required

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -2,9 +2,9 @@
 import { ThreeDots } from "react-loader-spinner";
 import { twMerge } from "tailwind-merge";
 
-const Loader = ({className}:{className:string}) => {
+const Loader = ({className}:{className?:string}) => {
   return (
-    <div className={twMerge("flex justify-center absolute items-center w-full h-full bg-white/10 rounded-xl cursor-progress",className.toString())}>
+    <div className={twMerge("flex justify-center absolute items-center w-full h-full bg-white/10 rounded-xl cursor-progress",className)}>
       <ThreeDots color="#00AEFF" height={80} width={80} />
     </div>
   );
