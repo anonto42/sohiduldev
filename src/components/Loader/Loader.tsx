b2cf5025fbeabed0ee4fbe5777bc4aaf9fa773ee
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-03-09 11:54:35 +0600

    trying to update the mouse status

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -4,7 +4,7 @@ import { twMerge } from "tailwind-merge";
 
 const Loader = ({className}:{className:string}) => {
   return (
-    <div className={twMerge("flex justify-center absolute items-center w-full h-full bg-white/10 rounded-xl",className.toString())}>
+    <div className={twMerge("flex justify-center absolute items-center w-full h-full bg-white/10 rounded-xl cursor-progress",className.toString())}>
       <ThreeDots color="#00AEFF" height={80} width={80} />
     </div>
   );
