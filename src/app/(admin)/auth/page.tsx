0b90053bec907a796511789a30255e9b28f104f7
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-03-09 19:54:07 +0600

    form acction configed for the login

--- src/app/(admin)/auth/page.tsx
+++ src/app/(admin)/auth/page.tsx
@@ -1,3 +1,4 @@
+import { gitHubLogin } from '@/app/actions';
 import React from 'react';
 import { FaGithub } from "react-icons/fa";
 
@@ -17,14 +18,17 @@ const page = () => {
                 >
                     Login with GitHub
                 </h2>
-
+                <form action={gitHubLogin}>
                     <button
+                        type='submit'
+                        name='action'
+                        value="github"
                         className='bg-[#010409] flex items-center justify-between gap-2 py-2 px-4 rounded-lg mx-auto active:bg-[#00000096] active:scale-[1.03] duration-100 ease-in-out shadow-md hover:shadow-[#0000005b]'
                     >
                         GitHub 
                         <FaGithub />
                     </button>
-            
+                </form>
             </div>
         </div>
     </div>
