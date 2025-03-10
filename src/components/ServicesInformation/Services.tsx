aeb37cc1546a61c9f17e838a23dad951d7012ef4
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-03-11 00:43:31 +0600

    animation added and the context updated

--- src/components/ServicesInformation/Services.tsx
+++ src/components/ServicesInformation/Services.tsx
@@ -1,13 +1,30 @@
+"use client"
 import React from 'react';
+import { motion } from 'framer-motion';
 
 const Services = () => {
   return (
-    <section className='h-[100px] w-full'>
-        <div className='w-full flex justify-center my-20 text-center text-topr'>
+    <section className='w-full py-2 md:py-8 md:mt-4'>
+        <div className='w-full flex justify-center text-center'>
+          <motion.div
+            initial={{
+              opacity:0,
+              y:20
+            }}
+            animate={{
+              opacity:1,
+              y:0
+            }}
+            transition={{
+              duration:1,
+              ease:"linear"
+            }}
+          >
             <div className=' bg-gradient-to-br from-white to-highlighte bg-clip-text text-transparent'>
-                <h1 className='text-2xl md:text-4xl lg:text-5xl font-bold'>I will create bespoke software solutions</h1>
-                <h4 className='text-2xl md:text-3xl lg:text-4xl font-semibold'>precisely tailored to meet your specific needs.</h4>
+                <h1 className='text-2xl md:text-3xl font-bold'>I will develop custom software,</h1>
+                <h4 className='text-xl md:text-2xl font-semibold'>precisely perfectly built to fit your needs.</h4>
             </div>
+          </motion.div>
         </div>
     </section>
   )
