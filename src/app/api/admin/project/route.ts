0288d3d3920b7200d67d9846925c5fd01b4b5034
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-03-10 19:44:43 +0600

    crateated the project upload funconality on the admin route

--- src/app/api/admin/project/route.ts
+++ src/app/api/admin/project/route.ts
@@ -2,24 +2,69 @@ import { auth } from "@/auth";
 import MongoConnect from "@/db/mongoDBConnent";
 import { ApiResponse } from "@/lib/ApiResponce";
 import ProjectModel from "@/model/project";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import streamifier from "streamifier";
+import { v2 as cloud } from "cloudinary";
 
+cloud.config({
+    cloud_name: process.env.CLD_NAME,
+    api_key: process.env.CLD_API_KEY,
+    api_secret: process.env.CLD_API_SECRET_KEY,
+});
 
 export async function POST(request: NextRequest) {
     await MongoConnect();
     try {
-        
-        const section = await auth();
-        if (section?.user?.email !== "anonto1080@gmail.com") {
-            return ApiResponse.error("This route is only for admin",408)
+        const session = await auth();
+        if (session?.user?.email !== "anonto1080@gmail.com") {
+            return ApiResponse.error("This route is only for admin", 408);
         }
-        
+
+        const fData = await request.formData();
+        if (!fData) return Response.json({ message: "No file found!" });
+
+        const file = fData.get("image") as unknown as File;
+        if (!file) return Response.json({ message: "Invalid file" });
+
+        const bytes = await file.arrayBuffer();
+        const bufferFile = Buffer.from(bytes);
+
+        const uploadResult = await new Promise((resolve, reject) => {
+            const uploadStream = cloud.uploader.upload_stream(
+                {
+                    folder: "portfolio",
+                    resource_type: "image",
+                },
+                (error, result) => {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        resolve(result);
+                    }
+                }
+            );
+
+            streamifier.createReadStream(bufferFile).pipe(uploadStream);
+        });
+
+        if (!uploadResult) {
+            return NextResponse.json({ error: "Problem in file upload!" }, { status: 500 });
+        }
+
+        const project = await ProjectModel.create({
+            title: fData.get("title"),
+            liveLink: fData.get("liveLink"),
+            frontImage: (uploadResult as any).secure_url,
+        });
+
+        return NextResponse.json({ message: "Project created", data: project }, { status: 200 });
 
     } catch (error) {
         console.error("Error:", error);
-        return ApiResponse.error("Something was wrong! on deleting the project", 500);
+        return ApiResponse.error("Something went wrong while creating the project", 500);
     }
 }
+
 
 export async function DELETE(request: NextRequest) {
     await MongoConnect();
--- src/components/Deshbord_Items/AddProject.tsx
+++ src/components/Deshbord_Items/AddProject.tsx
@@ -1,9 +1,10 @@
-"use state"
-import Loading from '@/app/loading';
+"use client"
 import Image from 'next/image';
 import React, { useState } from 'react'
 import Loader from '../Loader/Loader';
 import { toast } from 'react-toastify';
+import { SessionProvider } from "next-auth/react";
+import axios from 'axios';
 
 const AddProject = () => {
     const [file,setFile] = useState<File | null>(null);
@@ -15,92 +16,98 @@ const AddProject = () => {
     const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         if (e.target.files && e.target.files.length > 0) {
             const selectedFile = e.target.files[0];
-            setFile(e.target.files[0]);
+            setFile(selectedFile);
             const objectURL = URL.createObjectURL(selectedFile);
             setPreview(objectURL);
         }
     };
 
-    const handleUpload = ()=>{
+    const handleUpload = async ()=>{
+        setLoading(true)
         try {
-            if (!file && title.trim() === "" && liveLink.trim() === "" && liveLink.trim() === "") {
-                return toast.warning("all fields are required!");
+            if (!file || title.trim() === "" || liveLink.trim() === "") {
+                toast.warning("All fields are required!");
+                setLoading(false);
+                return;
             }
-            const data = {
-                frontImage: file,
-                name: title.trim(),
-                liveLink: liveLink.trim()
-            } 
 
-            
-        } catch (error) {
-            console.log(error)
+            const formData = new FormData();
+                  formData.append("image",file);
+                  formData.append("title",title.trim());
+                  formData.append("liveLink",liveLink.trim());
+
+            const { data } = await axios.post("/api/admin/project",formData,{headers:{"Content-Type":"multipart/form-data"}})
+
+            toast.success(data.message || "Your request success fully resved.")
+
+            setLoading(false)
+        } catch (error:any) {
+            console.log(error.response)
+            setLoading(false)
         }
-    }
+    };
 
   return (
     <section className='w-full h-svh'>
-        <div className='p-6'>
-
-
-            <div className='w-[700px] h-[650px] rounded-lg bg-[#00000042] shadow-[0px_0px_4px] relative mx-auto overflow-clip'>
-                { loading && <Loader className='' />}
-                <div >
-                    <div>
-                        <label htmlFor='fileSilectior'>
-                            <div className='w-full h-[400px] p-[100px] relative mx-auto cursor-pointer shadow-sm shadow-[#ffffff3f]'>
-                                {
-                                    preview ? (
-                                        <Image
-                                            src={preview} // Use the preview URL
-                                            alt="file preview"
-                                            fill
-                                            quality={60}
-                                            className="rounded-md -cover"
-                                        />
-                                    ) : (
-                                    <p className="text-gray-500 flex flex-col justify-center items-center text-3xl pt-[70px]">
-                                        No Image Selected
-                                        <span className='text-xl'>Click to select the image</span>
-                                    </p>
-                                    
-                                )}
+        <SessionProvider>
+            <div className='p-6'>
+                <div className='w-[700px] h-[650px] rounded-lg bg-[#00000042] shadow-[0px_0px_4px] relative mx-auto overflow-clip'>
+                        { loading && <Loader className='' />}
+                        <div >
+                            <div>
+                                <label htmlFor='fileSilectior'>
+                                    <div className='w-full h-[400px] p-[100px] relative mx-auto cursor-pointer shadow-sm shadow-[#ffffff3f]'>
+                                        {
+                                            preview ? (
+                                                <Image
+                                                    src={preview} // Use the preview URL
+                                                    alt="file preview"
+                                                    fill
+                                                    quality={60}
+                                                    className="rounded-md -cover"
+                                                />
+                                            ) : (
+                                            <p className="text-gray-500 flex flex-col justify-center items-center text-3xl pt-[70px]">
+                                                No Image Selected
+                                                <span className='text-xl'>Click to select the image</span>
+                                            </p>
+                                            
+                                        )}
+                                    </div>
+                                </label>
+                                <input 
+                                    className='hidden'
+                                    onChange={ handleFileChange }
+                                    type="file" 
+                                    id='fileSilectior'
+                                    />
                             </div>
-                        </label>
-                        <input 
-                            className='hidden'
-                            onChange={ handleFileChange }
-                            type="file" 
-                            id='fileSilectior'
-                            />
-                    </div>
-                    <div className='flex flex-col w-full h-auto gap-4 absolute bottom-3'>
-                        <input 
-                            className='w-[80%] h-[50px] mx-auto rounded-md bg-[#0605290e] shadow-white shadow-[0px_0px_10px] px-4 text-lg'
-                            placeholder='Inter the title..'
-                            type="text" 
-                            name="" 
-                            id=""
-                            onChange={ e => setTitle(e.target.value)}
-                        />
-                        <input 
-                            className='w-[80%] h-[50px] mx-auto rounded-md bg-[#0605290e] shadow-white shadow-[0px_0px_10px] px-4 text-lg'
-                            placeholder='Give the Live link'
-                            type="text" 
-                            name="" 
-                            id="" 
-                            onChange={ e => setTitle(e.target.value)}
-                        />
-                    <button 
-                        onClick={handleUpload}
-                        className='bg-blue-500 w-[110px] h-[40px] mx-auto rounded-md active:scale-95 duration-100 ease-in-out'
-                        >Upload</button>
-                    </div>
+                            <div className='flex flex-col w-full h-auto gap-4 absolute bottom-3'>
+                                <input 
+                                    className='w-[80%] h-[50px] mx-auto rounded-md bg-[#0605290e] shadow-white shadow-[0px_0px_10px] px-4 text-lg'
+                                    placeholder='Inter the title..'
+                                    type="text" 
+                                    name="" 
+                                    id=""
+                                    onChange={ e => setTitle(e.target.value)}
+                                />
+                                <input 
+                                    className='w-[80%] h-[50px] mx-auto rounded-md bg-[#0605290e] shadow-white shadow-[0px_0px_10px] px-4 text-lg'
+                                    placeholder='Give the Live link'
+                                    type="text" 
+                                    name="" 
+                                    id="" 
+                                    onChange={ e => setLiveLink(e.target.value)}
+                                />
+                            <button 
+                                onClick={handleUpload}
+                                className='bg-blue-500 w-[110px] h-[40px] mx-auto rounded-md active:scale-95 duration-100 ease-in-out'
+                                >Upload</button>
+                            </div>
+                        </div>
                 </div>
             </div>
-
-
-        </div>
+        </SessionProvider>
     </section>
   )
 }
